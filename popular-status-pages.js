6329bad78666f2d1466b8d3a3709b5c1e5b951c1
ericspence <eric.spence@genesys.com>
2019-08-31 11:34:53 -0400

    adding a popular tab

---
+++ popular-status-pages.js
@@ -0,0 +1,24 @@
+export const popularSites = {
+    "providers": [
+        "statusPageIo",
+        "google"
+    ],
+    "sites": [
+        {
+            "hostName": "https://status.cloud.google.com",
+            "provider": "google"
+        },
+        {
+            "hostName": "https://status.newrelic.com/",
+            "provider": "statusPageIo"
+        },
+        {
+            "hostName": "https://jira-software.status.atlassian.com/",
+            "provider": "statusPageIo"
+        },
+        {
+            "hostName": "https://www.githubstatus.com/",
+            "provider": "statusPageIo"
+        }
+    ]
+}
