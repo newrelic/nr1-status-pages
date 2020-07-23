3f96d59f4201234758343c7e87e2e12018baa6a6
artur.wozniak <wozniaka83@gmail.com>
2020-07-23 15:31:51 +0200

    fix: unknown EventStatus transformed into none

--- utilities/formatters/nrql.js
+++ utilities/formatters/nrql.js
@@ -5,12 +5,17 @@ const NRQLSeverityToKnown = {
 };
 
 export const nrqlFormatter = data => {
+  console.log('data', data);
   let statusCode = NRQLSeverityToKnown.None;
   let status = 'All Systems Operational';
 
   if (data.raw.results[0].events.length > 0) {
     const incident = data.raw.results[0].events[0];
     statusCode = NRQLSeverityToKnown[incident.EventStatus];
+
+    if (statusCode === undefined) {
+      statusCode = NRQLSeverityToKnown.None;
+    }
 
     if (statusCode !== NRQLSeverityToKnown.None) {
       status = 'Ongoing Issues';
@@ -27,6 +32,7 @@ export const nrqlFormatter = data => {
 export const nrqlIncidentFormatter = data => {
   return data.raw.results[0].events.map(incident => {
     const incident_updates = [];
+    let incidentCode = NRQLSeverityToKnown[incident.EventStatus];
 
     Object.entries(incident).forEach(([key, value]) => {
       incident_updates.push({
@@ -35,10 +41,14 @@ export const nrqlIncidentFormatter = data => {
       });
     });
 
+    if (incidentCode === undefined) {
+      incidentCode = NRQLSeverityToKnown.None;
+    }
+
     return {
       name: incident.EventName,
       created_at: incident.EventTimeStamp,
-      impact: NRQLSeverityToKnown[incident.EventStatus],
+      impact: incidentCode,
       incident_updates
     };
   });
