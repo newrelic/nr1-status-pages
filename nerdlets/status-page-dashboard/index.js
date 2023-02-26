349b41a89a58f898808cae38e5df06cdc95a9fda
shahram <shahram@newrelic.com>
2023-02-26 14:49:12 -0800

    feat: add HelpModal component

--- nerdlets/status-page-dashboard/index.js
+++ nerdlets/status-page-dashboard/index.js
@@ -1,21 +1,80 @@
 import React from 'react';
 import StatusPagesDashboard from './main-page';
-import { NerdletStateContext } from 'nr1';
+import { Icon, nerdlet, NerdletStateContext } from 'nr1';
+
+import { HelpModal } from 'nr-labs-components';
 
 export default class Wrapper extends React.PureComponent {
+  state = {
+    helpModalOpen: false
+  };
+
+  componentDidMount() {
+    nerdlet.setConfig({
+      actionControls: true,
+      actionControlButtons: [
+        {
+          label: 'Help',
+          hint: 'Quick links to get support',
+          type: 'primary',
+          iconType: Icon.TYPE.INTERFACE__INFO__HELP,
+          onClick: () => this.setHelpModalOpen(true)
+        }
+      ]
+    });
+  }
+
+  setHelpModalOpen = helpModalOpen => {
+    this.setState({ helpModalOpen });
+  };
+
   render() {
+    const { helpModalOpen } = this.state;
+
     return (
-      <NerdletStateContext.Consumer>
-        {nerdletUrlState => {
-          const { entityGuid } = nerdletUrlState;
-          const type = entityGuid ? 'entity' : 'account';
-          if (type === 'entity') {
-            return <StatusPagesDashboard entityGuid={entityGuid} />;
-          } else {
-            return <StatusPagesDashboard />;
-          }
-        }}
-      </NerdletStateContext.Consumer>
+      <>
+        <NerdletStateContext.Consumer>
+          {nerdletUrlState => {
+            const { entityGuid } = nerdletUrlState;
+            const type = entityGuid ? 'entity' : 'account';
+            if (type === 'entity') {
+              return <StatusPagesDashboard entityGuid={entityGuid} />;
+            } else {
+              return <StatusPagesDashboard />;
+            }
+          }}
+        </NerdletStateContext.Consumer>
+        {helpModalOpen && (
+          <HelpModal
+            isModalOpen={helpModalOpen}
+            setModalOpen={this.setHelpModalOpen}
+            urls={{
+              docs: 'https://github.com/newrelic/nr1-status-pages#readme',
+              createIssue:
+                'https://github.com/newrelic/nr1-status-pages/issues/new?assignees=&labels=bug%2C+needs-triage&template=bug_report.md&title=',
+              createFeature:
+                'https://github.com/newrelic/nr1-status-pages/issues/new?assignees=&labels=enhancement%2C+needs-triage&template=enhancement.md&title=',
+              createQuestion:
+                'https://github.com/newrelic/nr1-status-pages/discussions/new/choose'
+            }}
+            ownerBadge={{
+              logo: {
+                src:
+                  'https://drive.google.com/uc?id=1BdXVy2X34rufvG4_1BYb9czhLRlGlgsT',
+                alt: 'New Relic Labs'
+              },
+              blurb: {
+                text: 'This is a New Relic Labs open source app.',
+                link: {
+                  text: 'Take a look at our other repos',
+                  url:
+                    'https://github.com/newrelic?q=nrlabs-viz&type=all&language=&sort='
+                }
+              }
+            }}
+          />
+        )}
+      </>
     );
   }
 }
